c353151eb4de2b7733c7bb0a79f3c5b56b5846e5
= <=>
2026-05-16 12:49:57 +0530

    16-may-nineth-commit

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,22 @@
 const { defineConfig } = require('@playwright/test');
 const allure = require('allure-playwright/reporter');
+
 module.exports = defineConfig({
-    use: {
-        headless: true,
-        screenshot: 'on',
-        video: 'on',
-        trace:'on'
-    },
-    reporter: [
-        ['list'],
-        ['junit', { outputFile: 'results.xml' }],
-        ['html', { outputFolder: 'playwright-report', open: 'never' }],
-        ['allure-playwright', { resultsDir: 'allure-results' }]
-    ],
-    });
+  use: {
+    headless: true,
+    screenshot: 'on',
+    video: 'on',
+    trace: 'on'
+  },
+
+  reporter: [
+    ['list'],
+
+    ['junit', { outputFile: 'results.xml' }],
+
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+
+    // FIXED Allure integration
+    [allure, { outputFolder: 'allure-results' }]
+  ],
+});
